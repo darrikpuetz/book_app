cd036868745372756a1678bddce984ddb2010dc9
Scuba-Chris <chrisceder@hotmail.com>
2019-09-25 13:23:20 -0700

    most of features 1 and 2 completed

---
+++ data/books.sql
@@ -0,0 +1,26 @@
+
+DROP TABLE IF EXISTS books;
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  author VARCHAR(255),
+  title VARCHAR(255),
+  image_url TEXT,
+  isbn VARCHAR(255),
+  description TEXT,
+  bookshelf VARCHAR(255)
+);
+
+INSERT INTO books (author, title, image_url, isbn, description, bookshelf)  VALUES('Paul Harrison',
+'The Disappearing Cheese',
+'http://books.google.com/books/content?id=gF_5nml6D58C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+'ISBN_13 9780237527754',
+'One night a foolish man was walking past the sea. His tummy was as empty as his head. But is that a cheese he sees in the water? He decides to find out.',
+'Cheese Stuff');
+
+
+INSERT INTO books (author, title, image_url, isbn, description, bookshelf) VALUES ('J. R. R. Tolkien',
+'Who Cut the Cheese?',
+'http://books.google.com/books/content?id=G5rqlCg8AZEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+'ISBN_13 9781442433083',
+'"Large helpings of whimsy, humorous black-and-white illustrations, and the occasional fart joke provide plenty of silliness" (Booklist) in the third Doctor Proctor adventure from New York Times bestselling author Jo Nesbø. Nilly, Lisa, and Doctor Proctor are too busy inventing things to watch TV, and everyone says theyre missing out on the hot singing competition. But then Nilly and Lisa notice that their friends and family are acting really weird. And the only people acting weird…are the ones watching TV. Whats going on is WAY bigger than a singing competition. It could mean the end of the world. Or a silent but deadly could save everything! Let er rip.',
+'Funny');
--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 'use strict';
 
 const express = require('express');
-const superagent = require('superagent');
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+const pg = require('pg');
+const superagent = require('superagent');
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', problem => console.error(problem));
 
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', newSearch);
+app.get('/', grabFromBookshelf);
 
 app.post('/searches', searchMaker);
+app.post('/search', newSearch);
+app.post('/books', bookMaker);
+app.get('/books/:book_id', getBookFromTable);
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
@@ -25,6 +32,8 @@ function Book(info) {
   this.authors = info.authors ? info.authors : 'no author';
   this.description = info.description ? info.description : 'no description';
   this.image = info.imageLinks ? info.imageLinks.thumbnail : placeholderImage;
+  this.isbn = info.industryIdentifiers ? `ISBN_13 ${info.industryIdentifiers[0].identifier}` : 'No ISBN available';
+
   console.log(this.image = info.imageLinks ? info.imageLinks.thumbnail : placeholderImage);
 }
 
@@ -45,4 +54,42 @@ function searchMaker(request, response) {
     .then(apiCall => apiCall.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
     .then(books => response.render('pages/searches/show', { searchResults: books }))
     .catch(err => response.status(500).render('pages/error'), { err: 'Sorry page not found' });
+}
+
+function grabFromBookshelf(request, response) {
+  let SQL = 'SELECT * from books;';
+  return client.query(SQL)
+    .then(results => {
+      if (results.rows.rowCount === 0) {
+        response.render('pages/search');
+      } else {
+        response.render('pages/index', { books: results.rows })
+      }
+    })
+    .catch(error => response.status(500).render('pages/error'), { error: 'Stoooooooopid' });
+}
+
+function bookMaker(request, response) {
+  let lowerCaseShelf = request.body.bookshelf.toLowerCase();
+  let { title, author, isbn, image_url, description } = request.body;
+  let SQL = 'INSERT INTO books(title, author, isbn, image_url, description, bookshelf) VALUES($1, $2, $3, $4, $5, $6);';
+  let values = [title, author, isbn, image_url, description, lowerCaseShelf];
+  return client.query(SQL, values)
+    .then(() => {
+      SQL = 'SELECT * FROM books WHERE isbn=$1;';
+      values = [request.body.isbn];
+      return client.query(SQL, values)
+        .then(result => response.redirect(`/books/${result.rows[0].id}`))
+        .catch(error => response.status(500).render('pages/error'), { err: 'yea yea yea, it sucks, we know ' });
+    })
+    .catch(error => response.status(500).render('pages/error'), { err: 'yea yea yea, it sucks, we know' });
+}
+
+function getBookFromTable(request, response) {
+  let SQL = `SELECT * from books WHERE id=${request.params.book_id};`;
+  return client.query(SQL)
+    .then(info => {
+      response.render('pages/books/show', { book: result.rows[0] })
+    })
+    .catch(error => response.status(500).render('pages/error'), { error: 'yea yea yea, it sucks, we know' });
 }
